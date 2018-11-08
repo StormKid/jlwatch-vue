63325ccd652fd86662bab2ba1e7354848ed13e09
ke_li <stokid@126.com>
2018-11-08 17:50:50 +0800

    首次提交

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+     <img src="@/assets/img/home.jpg" :style="{height:height+'px',width:width+'px'}">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+       height:0,
+       width:0
+    }
+  },
+  mounted() {
+     this.height = window.innerHeight;
+     this.width = window.innerWidth
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
